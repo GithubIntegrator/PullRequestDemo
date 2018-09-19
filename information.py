42c45d87cf720500813bc0ff96d43480e031bbca
GithubIntegrator <43406158+GithubIntegrator@users.noreply.github.com>
2018-09-19 19:38:39 +0800

    Changes made accordingly
    I made change according to your suggestions

--- information.py
+++ information.py
@@ -1,3 +1,4 @@
 print("This is python code")
 print("This file is just used for the demo of pull request")
 print("This is the code added by the contributor")
+print("Ok! now please check this change I made")
